cfbb0a7aa9827872735cc8c62db5eb54f88dc62e
Victor Tesla <victor.tesla@grammm.com>
2021-05-08 10:14:56 +0200

    replace mattermost themes

--- src/types/config.ts
+++ src/types/config.ts
@@ -35,7 +35,7 @@ export type ClientConfig = {
     DataRetentionFileRetentionDays: string;
     DataRetentionMessageRetentionDays: string;
     DefaultClientLocale: string;
-    DefaultTheme: 'default' | 'organization' | 'mattermostDark' | 'windows10';
+    DefaultTheme: 'default' | 'organization' | 'grammmDark' | 'windows10';
     DesktopLatestVersion: string;
     DesktopMinVersion: string;
     DiagnosticId: string;
